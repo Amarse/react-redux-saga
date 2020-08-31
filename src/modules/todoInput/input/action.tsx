5e3826aa62a81d1e2d1886c432cbd0cf9ef3fe50
Amarse <byicy08@gmail.com>
2020-08-31 14:18:37 +0900

    commit test

--- src/modules/todoInput/input/action.tsx
+++ src/modules/todoInput/input/action.tsx
@@ -1,18 +1,15 @@
-//redux 만들기 
-
-
+//redux 만들기
 
 //1. type 정의
-export const INPUT = 'INPUT';
+export const INPUT = "INPUT";
 
 //2. action 정의
 export interface InputAction {
-    type: typeof INPUT;
-    payload: { value: string; };
+	type: typeof INPUT;
+	payload: { value: string };
 }
 
-
 export const input = (value: string): InputAction => ({
-    type: INPUT,
-    payload: { value: value }
+	type: INPUT,
+	payload: { value: value },
 });
